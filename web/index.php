ca8bd179958469991d38a47e6d9c845a592daedb
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2018-11-28 08:55:24 -0300

    Correccion de formulario
    Se corrige pretext de formularios

--- web/index.php
+++ web/index.php
@@ -406,15 +406,15 @@ La app contará con un sistema de puntuación y ranking entre los diferentes jug
                     <div class="modal-body">
                         <div class="form-group">
                             <label for="exampleFormControlInput1">Nombre</label>
-                            <input type="text" class="form-control" id="name" name="name" placeholder="Pato">
+                            <input type="text" class="form-control" id="name" name="name" placeholder="Usuario">
                         </div>
                         <div class="form-group">
                             <label for="exampleFormControlInput1">Edad</label>
-                            <input type="number" class="form-control" id="year" name="year" placeholder="22">
+                            <input type="number" class="form-control" id="year" name="year" placeholder="Edad">
                         </div>
                         <div class="form-group">
                             <label for="exampleFormControlInput1">Contraseña</label>
-                            <input type="password" class="form-control" id="password" name="password" placeholder="contraseña">
+                            <input type="password" class="form-control" id="password" name="password" placeholder="Contraseña">
                         </div>
                     </div>
                     <div class="modal-footer">
